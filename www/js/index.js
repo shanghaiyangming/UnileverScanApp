a9f62a7284ee346538ce6b0ffca7e79b9e79ca97
Young <shanghaiyangming@gmail.com>
2017-05-24 12:26:22 +0800

    debug

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,17 @@
-bluetoothSerial.list(function(data){
-	mac_address = data[0]['address'];
-	console.log('mac_adress is '+mac_address);
-	bluetoothSerial.connect(mac_address, function(){
-		bluetoothSerial.read(function (data) {
-			alert(data);
-		}, function(){
-			console.log('read failure');
-		});
-	}, function() {
-		console.log('connect failure');
-	};);
-}, function(){
-	console.log('fail get bluetooth list');
+document.addEventListener('deviceready', function() {
+	bluetoothSerial.list(function(data){
+		mac_address = data[0]['address'];
+		console.log('mac_adress is '+mac_address);
+		bluetoothSerial.connect(mac_address, function(){
+			bluetoothSerial.read(function (data) {
+				alert(data);
+			}, function(){
+				alert('read failure');
+			});
+		}, function() {
+			alert('connect failure');
+		};);
+	}, function(){
+		alert('fail get bluetooth list');
+	});
 });
